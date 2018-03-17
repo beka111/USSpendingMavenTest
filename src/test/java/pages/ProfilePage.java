7dbeb4e945f74d447053c8f093c98e56472796f0
temirleng <t.nurkulov@gmail.com>
2018-03-16 22:10:21 -0500

    Updated +READY smoke test

--- src/test/java/pages/ProfilePage.java
+++ src/test/java/pages/ProfilePage.java
@@ -1,14 +1,167 @@
 package pages;
 
+import java.util.List;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
 public class ProfilePage {
-	public ProfilePage() {
-		PageFactory federalPage = new PageFactory();
+	WebDriver driver;
 
+	public ProfilePage(WebDriver driver) {
+		this.driver = driver;
+		PageFactory.initElements(driver, this);
 	}
+
+	@FindBy(id = "logo")
+	WebElement logo;
+
+	/**
+	 * Verifying logo is displayed and Click on logo to verify it will navigate us
+	 * to home page
+	 */
+	public boolean logoIsThere() {
+		return logo.isDisplayed();
+	}
+
+	public void logoClick() {
+		logo.click();
+	}
+
+	@FindBy(css = ".official-banner__text")
+	WebElement officialWebSite;
+
+	public boolean officialWebSiteIsThere() {
+		return officialWebSite.isDisplayed();
+	}
+
+	/**
+	 * Spending Explorer link, click
+	 */
+	@FindBy(linkText = "Spending Explorer")
+	WebElement spendingExplorerLink;
+
+	public void spendingExplorerClick() {
+		spendingExplorerLink.click();
+	}
+
+	/**
+	 * Award Search dropdown links: Advanced Search, Keyword Search
+	 */
+
+	@FindBy(xpath = "//div[.='Award Search']")
+	WebElement awardSearch;
+
+	Actions action;
+
+	@FindBy(linkText = "Advanced Search")
+	WebElement advancedSearch;
+
+	public void advancedSearchClick() {
+		action = new Actions(driver);
+		action.moveToElement(awardSearch).perform();
+		advancedSearch.click();
+	}
+
+	@FindBy(linkText = "Keyword Search")
+	WebElement keywordSearch;
+
+	public void keywordSerachClick() {
+		action = new Actions(driver);
+		action.moveToElement(awardSearch).perform();
+		keywordSearch.click();
+	}
+
+	/**
+	 * Profiles dropdown links: Agencies, Federal Accounts
+	 */
+
+	@FindBy(xpath = "//div[.='Profiles']")
+	WebElement profiles;
+
+	@FindBy(xpath = "//a[.='Agencies']")
+	WebElement agenciesDropDown;
+
+	public void agenciesDropDownClick() {
+		action = new Actions(driver);
+		action.moveToElement(profiles).perform();
+		agenciesDropDown.click();
+	}
+
+	@FindBy(xpath = "//a[.='Federal Accounts']")
+	WebElement federalAccoutsDropDown;
+
+	public void federalAccoutsDropDownClick() {
+		action = new Actions(driver);
+		action.moveToElement(profiles).perform();
+		federalAccoutsDropDown.click();
+	}
+
+	/**
+	 * Download Center dropdown links: Award Data Archive, Custom Award Data, Agency
+	 * Submission Files, DataBase Snapshots, API
+	 */
+
+	@FindBy(xpath = "//div[.='Download Center']")
+	WebElement downloadCenter;
+
+	@FindBy(linkText = "Award Data Archive")
+	WebElement awardDataArchive;
+
+	public void awardDataArchiveClick() {
+		action.moveToElement(downloadCenter).perform();
+		awardDataArchive.click();
+	}
+
+	@FindBy(xpath = "//div[.='Custom Award Data']")
+	WebElement customAwardData;
+
+	public void customawardData() {
+		action.moveToElement(downloadCenter).perform();
+		customAwardData.click();
+	}
+
+	@FindBy(xpath = "//div[.='Agency Submission Files']")
+	WebElement agencySubmissionFiles;
+
+	public void agencySubmissionFiles() {
+		action.moveToElement(downloadCenter).perform();
+		agencySubmissionFiles.click();
+	}
+
+	@FindBy(xpath = "//div[.='Database Snapshots']")
+	WebElement databaseSnapshots;
+
+	public void databaseSnapshotsClick() {
+		action.moveToElement(downloadCenter).perform();
+		databaseSnapshots.click();
+	}
+
+	@FindBy(xpath = "//div[.='API']")
+	WebElement api;
+
+	public void apiClick() {
+		action.moveToElement(downloadCenter).perform();
+		api.click();
+	}
+
+	/**
+	 * Glossary
+	 */
+	@FindBy(xpath = "//div[.='Glossary']")
+	WebElement glossary;
+
+	public void glossaryClick() {
+		glossary.click();
+	}
+
+	/**
+	 * Text is present verification
+	 */
 
 	@FindBy(xpath = "//h1[contains(text(),'Federal Account Profiles')]")
 	WebElement fedAccoutProfiles;
@@ -27,11 +180,52 @@ public class ProfilePage {
 	@FindBy(xpath = "//ul[@class='pager']//preceding::div[@class='pagination']/div[@class='pagination__totals']")
 	WebElement results;
 
+	/**
+	 * Verifying the results, agencies table
+	 */
+
 	public boolean VerifyResults() {
 		return results.getText().equals("1-50 of 1668 results");
 	}
-	
-	
-	
-	
+
+	@FindBy(xpath = "//div[.='Account Number']")
+	WebElement accountNumber;
+
+	public void accountNumberClick() {
+		accountNumber.click();
+	}
+
+	@FindBy(xpath = "//div[.='Account Name']")
+	WebElement accountName;
+
+	public void accountNameClick() {
+		accountName.click();
+	}
+
+	@FindBy(xpath = "//div[.='Managing Agency']")
+	WebElement managingAgency;
+
+	public void managingAgencyClick() {
+		managingAgency.click();
+	}
+
+	@FindBy(xpath = "//div[.='2018 Budgetary Resources']")
+	WebElement budgetaryResources;
+
+	public void budgetaryRecourcesClick() {
+		budgetaryResources.click();
+	}
+
+	/**
+	 * Agencies list
+	 */
+	@FindBy(css = ".results-table-cell.results-table-cell_column_accountName")
+	WebElement agencies;
+
+	public List<WebElement> agenciesList() {
+		List<WebElement> list1 = driver
+				.findElements(By.cssSelector(".results-table-cell.results-table-cell_column_accountName"));
+		return list1;
+	}
+
 }
--- src/test/java/test/SmokeTest.java
+++ src/test/java/test/SmokeTest.java
@@ -1,5 +1,53 @@
 package test;
 
-public class SmokeTest {
+import java.io.IOException;
 
+import org.openqa.selenium.WebDriver;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import com.relevantcodes.extentreports.ExtentReports;
+import com.relevantcodes.extentreports.ExtentTest;
+
+import pages.ProfilePage;
+import utility.Config;
+import utility.Driver;
+
+public class SmokeTest {
+	WebDriver driver;
+	ProfilePage profPage;
+	ExtentReports report;
+	ExtentTest test;
+	
+	
+	@BeforeClass
+	public void setUp() throws IOException {
+		report=new ExtentReports("/USSpendingMavenTest/src/test/resources/SmokeTest.html");
+		test=report.startTest("Report for US Spending app");
+		driver=Driver.getDriver("chrome");
+		profPage=new ProfilePage(driver);
+		driver.get(Config.getPropValue("url"));
+	}
+	
+	@AfterClass
+	public void tearDown() {
+//		driver.quit();
+	}
+	@Test
+	public void test1() throws InterruptedException {
+		
+		profPage.spendingExplorerClick();
+		Thread.sleep(2000);
+		profPage.federalAccoutsDropDownClick();
+		profPage.keywordSerachClick();
+		Thread.sleep(2000);
+		profPage.agenciesDropDownClick();
+		Thread.sleep(2000);
+		profPage.awardDataArchiveClick();
+		Thread.sleep(2000);
+		profPage.glossaryClick();
+		Thread.sleep(2000);
+		profPage.logoClick();
+	}
 }
